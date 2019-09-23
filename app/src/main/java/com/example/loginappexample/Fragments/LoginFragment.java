29cde5c66c1225d28c6d348953b2aeb7515f7dc9
Sadia Islam <sadiaislam@gmail.com>
2019-09-24 00:35:10 +0600

    Register fragment working bt need to tested

--- app/src/main/java/com/example/loginappexample/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/loginappexample/Fragments/LoginFragment.java
@@ -1,13 +1,17 @@
 package com.example.loginappexample.Fragments;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.loginappexample.R;
 
@@ -16,6 +20,14 @@ import com.example.loginappexample.R;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+
+    private TextView textViewReg;
+    private OnLoginFormActivityListener onLoginFormActivityListener;
+
+    public interface OnLoginFormActivityListener{
+        public void performRegister();
+        public void performLogin(String name);
+    }
 
 
     public LoginFragment() {
@@ -27,7 +39,21 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        textViewReg = view.findViewById(R.id.textView_login);
+        textViewReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginFormActivityListener.performRegister();
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Activity activity = (Activity) context;
+        onLoginFormActivityListener = (OnLoginFormActivityListener) activity;
+    }
 }
--- app/src/main/java/com/example/loginappexample/MainActivity.java
+++ app/src/main/java/com/example/loginappexample/MainActivity.java
@@ -3,11 +3,41 @@ package com.example.loginappexample;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.loginappexample.Fragments.LoginFragment;
+import com.example.loginappexample.Fragments.RegisterFragment;
+import com.example.loginappexample.Fragments.WelcomeFragment;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnLoginFormActivityListener {
+
+    private static PrefConfig prefConfig;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefConfig = new PrefConfig(this);
+
+        if(findViewById(R.id.fragment_container) != null){
+            if(savedInstanceState != null){
+                return;
+            }
+            if(prefConfig.readLoginStatus()){
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new WelcomeFragment()).commit();
+            }else{
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new LoginFragment()).commit();
+            }
+        }
+    }
+
+    @Override
+    public void performRegister() {
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new RegisterFragment()).commit();
+    }
+
+    @Override
+    public void performLogin(String name) {
+
     }
 }
